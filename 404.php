1edbec5cc87e5f0c8b1619e1c9ed6b3195e246bf
Oliver Etchebarne <yo@drmad.org>
2018-09-08 23:05:52 -0500

    Una página más 404-ish :-)

--- 404.php
+++ 404.php
@@ -1,4 +1,18 @@
 <?php header ('HTTP/1.0 404 OMGWTFBBQ!!!1ONE');?>
-<html>
-<h1>404 OMGWTFBBQ!!!1ONE</h1>
-</html>
+<html><head>
+<style>
+    body {margin: 0px; font-family: sans-serif;}
+    main {position: absolute; top: 50%; left: 50%; transform: translateX(-50%) translateY(-50%); text-align: center}
+    .omg {font-family: serif; font-size:400px;}
+    .text {font-weight: bold;}
+    .link {padding: 10px;}
+    .link a {color: red; text-decoration: none;}
+    @media (max-width: 500px) {.omg {font-family: serif; font-size:200px;}}
+</style>
+</head><body>
+    <main>
+        <div class="omg">😱</div>
+        <div class="text">¡Omaiga! ¡Esta página no existe!</div>
+        <div class="link"><a href="https://icanux.org">&laquo; De regreso a ICANUX</a>
+    </main>
+</body></html>
