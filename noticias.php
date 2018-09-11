a1cc83cd7aa96fedca042930a3a9abafafec4de3
Elmer Ramos <elmerrl1996@gmail.com>
2018-09-11 12:54:55 -0500

    Iniciando el primer post de icanux - con la ayuda del gran Oliver

--- noticias.php
+++ noticias.php
@@ -2,9 +2,9 @@
 
 $menu = 'noticias';
 if (!isset($_SESSION['paginacion'])) {
-$_SESSION['paginacion'] = 0;
+	$_SESSION['paginacion'] = 0;
 } else if (isset($_SESSION['paginacion']) && $_SESSION['paginacion'] != 0) {
-$porpagina += $_SESSION['paginacion'];
+	$porpagina += $_SESSION['paginacion'];
 }
 
 $accion_post   = sprintf("SELECT * FROM post ORDER BY id DESC LIMIT $porpagina ");
@@ -14,69 +14,88 @@ $cantidad_post = mysqli_num_rows($consulta_post);
 require_once('inc/head.php');
 ?>
 <body>
-<?php include 'inc/header.php';?>
-<div class="ed-container">
-<div class="main-conent ed-container">
-<div class="ed-item m-75 l-75">
-<h1>POSTS</h1>
-<div id="listar">
-
-
-<?php
-
-if ($cantidad_post != 0) {
-	do {
-
-		$imagenes = $datos_post['image'];
-		$partes   = explode('####', $imagenes);
-
-		?>
-		<div class="ed-container">
-			<div class="ed-item l-25 m-25">
-
-				<?php if ($imagenes != '') {?>
-				<img src="static/img/upload/<?php echo $partes[0];?>">
-				<?php } else {?>
-				<img src="static/img/HELMI1.png">
-				<?php }?>
-
-			</div>
+	<?php include 'inc/header.php';?>
+	<div class="ed-container">
+		<div class="main-conent ed-container">
 			<div class="ed-item m-75 l-75">
-				<h2><a class="title-pub" href=" <?php echo $dato[0];?>post/<?php echo $datos_post['seo'];?> "><?php echo $datos_post['title'];
-					?></a> </h2>
-					<p class="content-pub">
-						<?php echo substr(strip_tags($datos_post['content']), 0, 1000);?></p><br><br>
-						<div class="etiqueta etiqueta-pequenia fecha">
-							<p>
-								<time datetime="<?php echo $datos_post['fecha'];?>">
-									Publicado el  dia <strong><?php echo date('d/m/Y H:i', strtotime($datos_post['fecha']));?></strong>
-								</time>
-							</p></div>
+				<div id="listar">
+					<br><br>
+					<div class="first-post">
+						<div class="ed-container">
+							<div class="ed-item l-25 m-25">
+								<img src="static/img/HELMI1.png">
+							</div>
+							<div class="ed-item m-75 l-75">
+
+								<h2><a class="title-pub">  ¡Bienvenidos a Icanux! </a> </h2>
+									<p class="content-pub">
+										Este primer post marca el inicio de nuestro nuevo sitio web, y el punto de partida para las posteriores secciones de noticias que estaremos compartiendo e informando a toda la comunidad de Linux, y del software libre.
+									</p>
+									<p>¡Nos estamos leyendo! <br><br>
+									- La familia ICANUX</p>
+										<img src="static/img/icanux.jpg" style="border-radius: 10px;"> 
+									<br><br>
+								</div>
+
+							</div>
 						</div>
+						<?php
 
-					</div>
+						if ($cantidad_post != 0) {
+							do {
 
-					<?php } while ($datos_post = mysqli_fetch_assoc($consulta_post));
-				}?>
-			</div>
+								$imagenes = $datos_post['image'];
+								$partes   = explode('####', $imagenes);
 
-			<div class="texto-centrado main-center" id="cargar">
-				<a onclick="paginar();" class="button button-enviar" id="ocultar">cargar mas</a>
-			</div>
-			<br>
-		</div>
+								?>
+								<div class="ed-container">
+									<div class="ed-item l-25 m-25">
 
-		<aside class="ed-item m-25 l-25">
+										<?php if ($imagenes != '') {?>
+										<img src="static/img/upload/<?php echo $partes[0];?>">
+										<?php } else {?>
+										<img src="static/img/HELMI1.png">
+										<?php }?>
+
+									</div>
+									<div class="ed-item m-75 l-75">
+										<h2><a class="title-pub" href=" <?php echo $dato[0];?>post/<?php echo $datos_post['seo'];?> "><?php echo $datos_post['title'];
+											?></a> </h2>
+											<p class="content-pub">
+												<?php echo substr(strip_tags($datos_post['content']), 0, 1000);?></p><br><br>
+												<div class="etiqueta etiqueta-pequenia fecha">
+													<p>
+														<time datetime="<?php echo $datos_post['fecha'];?>">
+															Publicado el  dia <strong><?php echo date('d/m/Y H:i', strtotime($datos_post['fecha']));?></strong>
+														</time>
+													</p></div>
+												</div>
+
+											</div>
+
+											<div class="texto-centrado main-center" id="cargar">
+												<a onclick="paginar();" class="button button-enviar" id="ocultar">cargar mas</a>
+											</div>
+
+											<?php } while ($datos_post = mysqli_fetch_assoc($consulta_post));
+										}?>
+									</div>
 
 
-			<?php include ('inc/sidebar.php');?>
-		</aside>
+									<br>
+								</div>
 
-	</div>
-</div>
-	<?php include 'inc/footer.php';?>
-</body>
-</html>
+								<aside class="ed-item m-25 l-25">
 
 
-<?php mysqli_free_result($consulta_post);
+									<?php include ('inc/sidebar.php');?>
+								</aside>
+
+							</div>
+						</div>
+						<?php include 'inc/footer.php';?>
+					</body>
+					</html>
+
+
+					<?php mysqli_free_result($consulta_post);
